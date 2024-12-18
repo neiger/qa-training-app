0a08220b5ddd42b5e124f6c84870291fee0a8fda
neiger <neiger.serrano.wks@gmail.com>
2024-12-18 16:09:19 -0600

    Refactor and improve methods in the calculator

--- app/src/main/java/com/qa/test/mycalculator/MainActivity.kt
+++ app/src/main/java/com/qa/test/mycalculator/MainActivity.kt
@@ -1,344 +1,166 @@
 package com.qa.test.mycalculator
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.util.TypedValue
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private var b1: Button? = null
-    private var b2: Button? = null
-    private var b3: Button? = null
-    private var b4: Button? = null
-    private var b5: Button? = null
-    private var b6: Button? = null
-    private var b7: Button? = null
-    private var b8: Button? = null
-    private var b9: Button? = null
-    private var b0: Button? = null
-    private var b_equal: Button? = null
-    private var b_multi: Button? = null
-    private var b_divide: Button? = null
-    private var b_add: Button? = null
-    private var b_sub: Button? = null
-    private var b_clear: Button? = null
-    private var b_dot: Button? = null
-    private var b_para1: Button? = null
-    private var b_para2: Button? = null
-    private var t1: TextView? = null
-    private var t2: TextView? = null
-    private val ADDITION = '+'
-    private val SUBTRACTION = '-'
-    private val MULTIPLICATION = '*'
-    private val DIVISION = '/'
-    private val EQU = '='
-    private val EXTRA = '@'
-    private val MODULUS = '%'
-    private var ACTION = 0.toChar()
-    private var val1 = Double.NaN
-    private var val2 = 0.0
 
-    @SuppressLint("SetTextI18n")
-    protected override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var buttons: Map<Int, Button>
+    private lateinit var inputDisplay: TextView
+    private lateinit var outputDisplay: TextView
+
+    private var valueOne = Double.NaN
+    private var valueTwo = 0.0
+    private var currentAction = NONE
+
+    companion object {
+        private const val ADDITION = '+'
+        private const val SUBTRACTION = '-'
+        private const val MULTIPLICATION = '*'
+        private const val DIVISION = '/'
+        private const val PERCENT = '%'
+        private const val POSITIVE_NEGATIVE = "+/-"
+        private const val NONE = ' '
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewSetup()
 
-        b1!!.setOnClickListener {
-            ifErrorOnOutput()
-            exceedLength()
-            t1!!.text = t1!!.text.toString() + "1"
-        }
+        initializeViews()
+        setButtonListeners()
+    }
 
-        b2!!.setOnClickListener {
-            ifErrorOnOutput()
-            exceedLength()
-            t1!!.text = t1!!.text.toString() + "2"
-        }
+    private fun initializeViews() {
+        buttons = mapOf(
+            R.id.button0 to findViewById(R.id.button0),
+            R.id.button1 to findViewById(R.id.button1),
+            R.id.button2 to findViewById(R.id.button2),
+            R.id.button3 to findViewById(R.id.button3),
+            R.id.button4 to findViewById(R.id.button4),
+            R.id.button5 to findViewById(R.id.button5),
+            R.id.button6 to findViewById(R.id.button6),
+            R.id.button7 to findViewById(R.id.button7),
+            R.id.button8 to findViewById(R.id.button8),
+            R.id.button9 to findViewById(R.id.button9),
+            R.id.button_add to findViewById(R.id.button_add),
+            R.id.button_sub to findViewById(R.id.button_sub),
+            R.id.button_multi to findViewById(R.id.button_multi),
+            R.id.button_divide to findViewById(R.id.button_divide),
+            R.id.button_percent to findViewById(R.id.button_percent),
+            R.id.button_postive_negative to findViewById(R.id.button_postive_negative),
+            R.id.button_dot to findViewById(R.id.button_dot),
+            R.id.button_clear to findViewById(R.id.button_clear),
+            R.id.button_equal to findViewById(R.id.button_equal)
+        )
 
-        b3!!.setOnClickListener {
-            ifErrorOnOutput()
-            exceedLength()
-            t1!!.text = t1!!.text.toString() + "3"
-        }
+        inputDisplay = findViewById(R.id.input)
+        outputDisplay = findViewById(R.id.output)
+    }
 
-        b4!!.setOnClickListener {
-            ifErrorOnOutput()
-            exceedLength()
-            t1!!.text = t1!!.text.toString() + "4"
-        }
-
-        b5!!.setOnClickListener {
-            ifErrorOnOutput()
-            exceedLength()
-            t1!!.text = t1!!.text.toString() + "5"
-        }
-
-        b6!!.setOnClickListener {
-            ifErrorOnOutput()
-            exceedLength()
-            t1!!.text = t1!!.text.toString() + "6"
-        }
-
-        b7!!.setOnClickListener {
-            ifErrorOnOutput()
-            exceedLength()
-            t1!!.text = t1!!.text.toString() + "7"
-        }
-
-        b8!!.setOnClickListener {
-            ifErrorOnOutput()
-            exceedLength()
-            t1!!.text = t1!!.text.toString() + "8"
-        }
-
-        b9!!.setOnClickListener {
-            ifErrorOnOutput()
-            exceedLength()
-            t1!!.text = t1!!.text.toString() + "9"
-        }
-
-        b0!!.setOnClickListener {
-            ifErrorOnOutput()
-            exceedLength()
-            t1!!.text = t1!!.text.toString() + "0"
-        }
-
-        b_dot!!.setOnClickListener {
-            exceedLength()
-            t1!!.text = t1!!.text.toString() + "."
-        }
-
-        b_para1!!.setOnClickListener {
-            if (t1!!.text.length > 0) {
-                ACTION = MODULUS
-                operation()
-                if (!ifReallyDecimal()) {
-                    t2!!.text = "$val1%"
-                } else {
-                    t2!!.text = val1.toInt().toString() + "%"
-                }
-                t1!!.setText(null)
-            } else {
-                t2!!.text = "Error"
-            }
-        }
-
-        b_add!!.setOnClickListener {
-            if (t1!!.text.length > 0) {
-                ACTION = ADDITION
-                operation()
-                if (!ifReallyDecimal()) {
-                    t2!!.text = "$val1+"
-                } else {
-                    t2!!.text = val1.toInt().toString() + "+"
-                }
-                t1!!.setText(null)
-            } else {
-                t2!!.text = "Error"
-            }
-        }
-
-        b_sub!!.setOnClickListener {
-            if (t1!!.text.length > 0) {
-                ACTION = SUBTRACTION
-                operation()
-                if (t1!!.text.length > 0) if (!ifReallyDecimal()) {
-                    t2!!.text = "$val1-"
-                } else {
-                    t2!!.text = val1.toInt().toString() + "-"
-                }
-                t1!!.setText(null)
-            } else {
-                t2!!.text = "Error"
-            }
-        }
-
-        b_multi!!.setOnClickListener {
-            if (t1!!.text.length > 0) {
-                ACTION = MULTIPLICATION
-                operation()
-                if (!ifReallyDecimal()) {
-                    t2!!.text = "$val1×"
-                } else {
-                    t2!!.text = val1.toInt().toString() + "×"
-                }
-                t1!!.setText(null)
-            } else {
-                t2!!.text = "Error"
-            }
-        }
-
-        b_divide!!.setOnClickListener {
-            if (t1!!.text.length > 0) {
-                ACTION = DIVISION
-                operation()
-                if (ifReallyDecimal()) {
-                    t2!!.text = val1.toInt().toString() + "/"
-                } else {
-                    t2!!.text = "$val1/"
-                }
-                t1!!.setText(null)
-            } else {
-                t2!!.text = "Error"
-            }
-        }
-
-        b_para2!!.setOnClickListener {
-            if (!t2!!.text.toString().isEmpty() || !t1!!.text.toString().isEmpty()) {
-                val1 = t1!!.text.toString().toDouble()
-                ACTION = EXTRA
-                t2!!.text = "-" + t1!!.text.toString()
-                t1!!.text = ""
-            } else {
-                t2!!.text = "Error"
-            }
-        }
-
-        b_equal!!.setOnClickListener {
-            if (t1!!.text.length > 0) {
-                operation()
-                ACTION = EQU
-                if (!ifReallyDecimal()) {
-                    t2!!.text = val1.toString()
-                } else {
-                    t2!!.text = val1.toInt().toString()
-                }
-                t1!!.setText(null)
-            } else {
-                t2!!.text = "Error"
-            }
-        }
-
-        b_clear!!.setOnClickListener {
-            if (t1!!.text.length > 0) {
-                val name: CharSequence = t1!!.text.toString()
-                t1!!.text = name.subSequence(0, name.length - 1)
-            } else {
-                val1 = Double.NaN
-                val2 = Double.NaN
-                t1!!.text = ""
-                t2!!.text = ""
-            }
-        }
-
-        // Empty text views on long click.
-        b_clear!!.setOnLongClickListener {
-            val1 = Double.NaN
-            val2 = Double.NaN
-            t1!!.text = ""
-            t2!!.text = ""
-            true
+    private fun setButtonListeners() {
+        buttons.forEach { (id, button) ->
+            button.setOnClickListener { handleButtonClick(id) }
         }
     }
 
-    private fun viewSetup() {
-        b1 = findViewById(R.id.button1)
-        b2 = findViewById(R.id.button2)
-        b3 = findViewById(R.id.button3)
-        b4 = findViewById(R.id.button4)
-        b5 = findViewById(R.id.button5)
-        b6 = findViewById(R.id.button6)
-        b7 = findViewById(R.id.button7)
-        b8 = findViewById(R.id.button8)
-        b9 = findViewById(R.id.button9)
-        b0 = findViewById(R.id.button0)
-        b_equal = findViewById(R.id.button_equal)
-        b_multi = findViewById(R.id.button_multi)
-        b_divide = findViewById(R.id.button_divide)
-        b_add = findViewById(R.id.button_add)
-        b_sub = findViewById(R.id.button_sub)
-        b_clear = findViewById(R.id.button_clear)
-        b_dot = findViewById(R.id.button_dot)
-        b_para1 = findViewById(R.id.button_para1)
-        b_para2 = findViewById(R.id.button_para2)
-        t1 = findViewById(R.id.input)
-        t2 = findViewById(R.id.output)
+    private fun handleButtonClick(buttonId: Int) {
+        when (buttonId) {
+            R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
+            R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9 -> {
+                appendToInput(buttons[buttonId]?.text.toString())
+            }
+            R.id.button_add -> prepareOperation(ADDITION)
+            R.id.button_sub -> prepareOperation(SUBTRACTION)
+            R.id.button_multi -> prepareOperation(MULTIPLICATION)
+            R.id.button_divide -> prepareOperation(DIVISION)
+            R.id.button_percent -> prepareOperation(PERCENT)
+            R.id.button_dot -> appendToInput(".")
+            R.id.button_clear -> clearInput()
+            R.id.button_equal -> calculateResult()
+        }
     }
 
-    private fun operation() {
-        if (!java.lang.Double.isNaN(val1)) {
-            if (t2!!.text.toString()[0] == '-') {
-                val1 = (-1) * val1
-            }
-            val2 = t1!!.text.toString().toDouble()
+    private fun appendToInput(value: String) {
+        val currentText = inputDisplay.text.toString()
+        inputDisplay.text = getString(R.string.concat_text_placeholder, currentText, value)
+        adjustFontSize()
+    }
 
-            when (ACTION) {
-                ADDITION -> val1 = val1 + val2
-                SUBTRACTION -> val1 = val1 - val2
-                MULTIPLICATION -> val1 = val1 * val2
-                DIVISION -> val1 = val1 / val2
-                EXTRA -> val1 = (-1) * val1
-                MODULUS -> val1 = val1 % val2
-                EQU -> {}
-            }
+    private fun prepareOperation(action: Char) {
+        if (!valueOne.isNaN()) {
+            valueTwo = parseInput()
+            performOperation()
         } else {
-            val1 = t1!!.text.toString().toDouble()
+            valueOne = parseInput()
         }
+
+        currentAction = action
+        val resultText = getString(R.string.operation_display, formatValue(valueOne), action)
+        outputDisplay.text = resultText
+        inputDisplay.text = ""
     }
 
-    // Remove error message that is already written there.
-    private fun ifErrorOnOutput() {
-        if (t2!!.text.toString() == "Error") {
-            t2!!.text = ""
-        }
-    }
+    private fun calculateResult() {
+        if (currentAction != NONE) {
+            valueTwo = parseInput()
 
-    // Whether value if a double or not
-    private fun ifReallyDecimal(): Boolean {
-        return val1 == val1.toInt().toDouble()
-    }
-
-    private fun noOperation() {
-        var inputExpression = t2!!.text.toString()
-        if (!inputExpression.isEmpty() && inputExpression != "Error") {
-            if (inputExpression.contains("-")) {
-                inputExpression = inputExpression.replace("-", "")
-                t2!!.text = ""
-                val1 = inputExpression.toDouble()
-            }
-            if (inputExpression.contains("+")) {
-                inputExpression = inputExpression.replace("+", "")
-                t2!!.text = ""
-                val1 = inputExpression.toDouble()
-            }
-            if (inputExpression.contains("/")) {
-                inputExpression = inputExpression.replace("/", "")
-                t2!!.text = ""
-                val1 = inputExpression.toDouble()
-            }
-            if (inputExpression.contains("%")) {
-                inputExpression = inputExpression.replace("%", "")
-                t2!!.text = ""
-                val1 = inputExpression.toDouble()
-            }
-            if (inputExpression.contains("×")) {
-                inputExpression = inputExpression.replace("×", "")
-                t2!!.text = ""
-                val1 = inputExpression.toDouble()
+            // Make sure the operation is valid and not dividing by zero
+            if (currentAction == DIVISION && valueTwo == 0.0) {
+                outputDisplay.text = getString(R.string.error_divide_by_zero)
+            } else {
+                performOperation()
+                outputDisplay.text = formatValue(valueOne)
+                inputDisplay.text = ""
+                currentAction = NONE
             }
         }
     }
 
-    //    // Make text small if too many digits.
-//    private fun exceedLength() {
-//        if (t1!!.text.toString().length > 10) {
-//            t1!!.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
-//        }
-//    }
-// Make text small if too many digits.
-    private fun exceedLength() {
-        val localT1 = t1 // Create a local reference for smart casting
-        if (localT1 is TextView) {
-            if (localT1.text.toString().length > 10) {
-                localT1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
-            }
+    private fun clearInput() {
+        inputDisplay.text = ""
+        outputDisplay.text = ""
+        valueOne = Double.NaN
+        valueTwo = 0.0
+        currentAction = NONE
+    }
+
+    private fun performOperation() {
+        valueOne = when (currentAction) {
+            ADDITION -> valueOne + valueTwo
+            SUBTRACTION -> valueOne - valueTwo
+            MULTIPLICATION -> valueOne * valueTwo
+            DIVISION -> if (valueTwo != 0.0) valueOne / valueTwo else Double.NaN
+            PERCENT -> valueOne * valueTwo / 100
+            else -> valueOne
+        }
+    }
+
+    private fun parseInput(): Double {
+        val input = inputDisplay.text.toString()
+        return input.toDoubleOrNull() ?: 0.0
+    }
+
+    private fun formatValue(value: Double): String {
+        return if (value == value.toLong().toDouble()) {
+            value.toLong().toString()
         } else {
-            Log.e("MainActivity", "t1 is not a TextView")
+            value.toString()
         }
+    }
+
+    // Adding more checks for the input size
+    private fun adjustFontSize() {
+        val textLength = inputDisplay.text.toString().length
+        val newSize = when {
+            textLength > 15 -> 20f
+            textLength > 10 -> 25f
+            else -> 30f
+        }
+        inputDisplay.setTextSize(TypedValue.COMPLEX_UNIT_SP, newSize)
     }
 
 }
